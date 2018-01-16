b8668e74c6c052a3729af9aec1e19c7ea72cbae3
Quyen <quyendvd00557@fpt.edu.vn>
2018-01-16 18:28:02 +0700

    change color

--- fontend/js/script.js
+++ fontend/js/script.js
@@ -2,7 +2,49 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.controller('ctrlHead', function($scope, $http){
     
 });
+app.controller('ctrlHead', function($scope, $http){
 
+    setInterval(function() { 
+        $('.mainSlide2').fadeIn(1000);
+        setTimeout(function(){
+            $('.mainSlide2').fadeOut(1000);
+        }, 4000);
+        setTimeout(function(){
+            $('.mainSlide3').fadeIn(1000);
+        }, 4000);
+        setTimeout(function(){
+            $('.mainSlide3').fadeOut(1000);
+        }, 9000);
+    },  12000);
+
+    setInterval(function() { 
+        $('.mainSlide5').fadeIn(1000);
+        setTimeout(function(){
+            $('.mainSlide5').fadeOut(1000);
+        }, 3000);
+        setTimeout(function(){
+            $('.mainSlide6').fadeIn(1000);
+        }, 3000);
+        setTimeout(function(){
+            $('.mainSlide6').fadeOut(1000);
+        }, 6000);
+    },  8000);
+
+    setInterval(function() { 
+        $('.mainSlide8').fadeIn(1000);
+        setTimeout(function(){
+            $('.mainSlide8').fadeOut(1000);
+        }, 5000);
+        setTimeout(function(){
+            $('.mainSlide9').fadeIn(1000);
+        }, 5000);
+        setTimeout(function(){
+            $('.mainSlide9').fadeOut(1000);
+        }, 10000);
+    },  15000);
+
+
+});
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
