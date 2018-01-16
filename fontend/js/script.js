976878152a1942ef4658802507156f838216ef19
Quyen <quyendvd00557@fpt.edu.vn>
2018-01-16 10:06:18 +0700

    festival

--- fontend/js/script.js
+++ fontend/js/script.js
@@ -8,5 +8,22 @@ app.config(function($routeProvider) {
     .when("/", {
         templateUrl : "chucnang/main.html"
     })
-    
+    .when("/detail", {
+        templateUrl : "chucnang/detail.html"
+    })
+    .when("/media", {
+        templateUrl : "chucnang/media.html"
+    })
+    .when("/about", {
+        templateUrl : "chucnang/about.html"
+    })
+    .when("/contact", {
+        templateUrl : "chucnang/contact.html"
+    })
+    .when("/login", {
+        templateUrl : "chucnang/login.html"
+    })
+    .when("/register", {
+        templateUrl : "chucnang/register.html"
+    })
 });
