99e1fd78a7ea7ef67fdb1a4719e76f45e2929b87
Nguyen Hong Hanh <hanhnhd00567@fpt.edu.vn>
2018-01-31 10:50:02 +0700

    validate ngày sinh của form register và form add user

--- register.js
+++ register.js
@@ -1,17 +1,28 @@
 var app = angular.module("myApp", ["ngRoute"]);
 app.controller('ctrlRegister', function($scope, $http){
 	$scope.btnSbm = function(){
+        var day = $scope.data.birthday.getTime();
+        var today = new Date();
+        today=today.getTime();
+        if(today>day){
+            $http({
+                method : "POST",
+                url : "http://localhost:3000/api/users",
+                data: $scope.data
+            }).then(function mySuccess(response) {
+                alert(Thêm Tài khoản thành công);
+                console.log(response);
+                window.location.href = "login.html";
+            }, function myError(response) {
+                alert(response.data);
+            });
+        }
+        else{
+            alert("Bạn không được chọn ngày tương lai !");
+        }
 		console.log($scope.data);
-		$http({
-            method : "POST",
-            url : "http://localhost:3000/api/users",
-            data: $scope.data
-        }).then(function mySuccess(response) {
-            console.log(response);
-            window.location.href = "login.html";
-        }, function myError(response) {
-            console.log(response.statusText);
-        });
+		
 	}
+    
     
 });
